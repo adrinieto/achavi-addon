3236efd2095fb588864c350176ea7ebfc5b011fd
Adrián Nieto <adrian.nieto7@gmail.com>
2017-06-14 23:08:48 +0200

    Improve script to work when opening the history sidebar and when loading more changesets

--- achavi.js
+++ achavi.js
@@ -1,19 +1,8 @@
-console.log("start");
 var ACHAVI_URL = 'https://overpass-api.de/achavi/?changeset='
-
-var changesets = document.querySelector('#sidebar_content ol.changesets')
-                .getElementsByTagName("li");
-
-
-for (var i = 0; i < changesets.length; i++) {
-    var changeset_id = changesets[i].id.replace("changeset_","");
-    var achavi_link = createLink(changeset_id);
-    changesets[i].querySelector(".details").appendChild(achavi_link);
-}
 
 function createLink(changeset_id) {
     var achavi_link = document.createElement("a")
-    achavi_link.text = "Open in Achavi"
+    achavi_link.text = "Achavi"
     achavi_link.href = ACHAVI_URL + changeset_id;
     achavi_link.setAttribute('target', '_blank');
     achavi_link.style.background = "#7092FF";
@@ -24,3 +13,64 @@ function createLink(changeset_id) {
     return achavi_link;
 }
 
+function search_changesets() {
+    var changesetsList = document.querySelectorAll('#sidebar_content ol.changesets');
+
+    for (var j = 0; j < changesetsList.length; j++){
+        var changesets = changesetsList[j].getElementsByTagName("li");
+        for (var i = 0; i < changesets.length; i++) {
+            var details = changesets[i].querySelector(".details");
+            //Don't add the button again
+            if (details.innerHTML.indexOf("Achavi") != -1) {
+                continue;
+            }
+            
+            var changeset_id = changesets[i].id.replace("changeset_","");
+            var achavi_link = createLink(changeset_id);
+            details.innerHTML += " · ";
+            details.appendChild(achavi_link);
+        }
+    }
+}
+
+/**
+ * Add a listener to the sidebar changes
+ */
+function addSidebarObserver() {
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.type == 'attributes'){
+                return;
+            }
+            search_changesets();
+            addChangesetsObserver();
+        });    
+    });
+
+    var config = { attributes: true, childList: true, characterData: true };
+    var changesets = document.querySelector('#sidebar_content');
+    observer.observe(changesets, config);
+}
+
+/**
+ * Add a listener to the changeset list
+ */
+function addChangesetsObserver() {
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            console.log(mutation.type);
+            if (mutation.type == 'attributes'){
+                return;
+            }
+            search_changesets();
+        });    
+    });
+
+    var config = { attributes: true, childList: true, characterData: true };
+    var changesets = document.querySelector('#sidebar_content div.changesets');
+    observer.observe(changesets, config);
+}
+
+search_changesets();
+addSidebarObserver();
+addChangesetsObserver();
