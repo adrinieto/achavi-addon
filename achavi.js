80dc298fedc11ef9fa396c8f64861dc219461640
Adrián Nieto <adrian.nieto7@gmail.com>
2017-06-16 00:17:45 +0200

    Reformat and  change variable names to camelCase

--- achavi.js
+++ achavi.js
@@ -1,22 +1,22 @@
 var ACHAVI_URL = 'https://overpass-api.de/achavi/?changeset='
 
-function createLink(changeset_id) {
-    var achavi_link = document.createElement("a")
-    achavi_link.text = "Achavi";
-    achavi_link.href = ACHAVI_URL + changeset_id;
-    achavi_link.setAttribute('target', '_blank');
-    achavi_link.style.background = "#7092FF";
-    achavi_link.style.color = "#eee";
-    achavi_link.style.fontWeight = "bold";
-    achavi_link.style.padding = "2px";
-    achavi_link.style.borderRadius = "4px";
-    return achavi_link;
+function createLink(changesetId) {
+    var achaviLink = document.createElement("a")
+    achaviLink.text = "Achavi";
+    achaviLink.href = ACHAVI_URL + changesetId;
+    achaviLink.setAttribute('target', '_blank');
+    achaviLink.style.background = "#7092FF";
+    achaviLink.style.color = "#eee";
+    achaviLink.style.fontWeight = "bold";
+    achaviLink.style.padding = "2px";
+    achaviLink.style.borderRadius = "4px";
+    return achaviLink;
 }
 
-function search_changesets() {
-    var changesetsList = document.querySelectorAll('#sidebar_content ol.changesets');
+function searchChangesets() {
+    var changesetsList = document.querySelectorAll('#sidebarContent ol.changesets');
 
-    for (var j = 0; j < changesetsList.length; j++){
+    for (var j = 0; j < changesetsList.length; j++) {
         var changesets = changesetsList[j].getElementsByTagName("li");
         for (var i = 0; i < changesets.length; i++) {
             var details = changesets[i].querySelector(".details");
@@ -24,30 +24,28 @@ function search_changesets() {
             if (details.innerHTML.indexOf("Achavi") != -1) {
                 continue;
             }
-            
-            var changeset_id = changesets[i].id.replace("changeset_","");
-            var achavi_link = createLink(changeset_id);
+
+            var changesetId = changesets[i].id.replace("changeset_", "");
+            var achaviLink = createLink(changesetId);
             details.innerHTML += " · ";
-            details.appendChild(achavi_link);
+            details.appendChild(achaviLink);
         }
     }
 }
 
 function showButtonInChangesetDetailPage() {
-    console.log("showButtonInChangesetDetailPage");
-    var changeset_id = window.location.pathname.replace("/changeset/", "");
+    var changesetId = window.location.pathname.replace("/changeset/", "");
 
     var details = document.querySelector('#sidebar_content h2');
-    var achavi_link = createLink(changeset_id);
-    details.appendChild(achavi_link);
-    console.log("aaa");
+    var achaviLink = createLink(changesetId);
+    details.appendChild(achaviLink);
 }
 
 function scanPage() {
     if (isChangesetDetailPage()) {
         showButtonInChangesetDetailPage();
     } else {
-        search_changesets();
+        searchChangesets();
         addChangesetsObserver();
     }
 }
@@ -62,18 +60,18 @@ function isChangesetDetailPage() {
  * on a changeset
  */
 function addSidebarObserver() {
-    var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            if (mutation.type == 'attributes'){
+    var observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            if (mutation.type == 'attributes') {
                 return;
             }
             scanPage();
-        });    
+        });
     });
 
     var config = { attributes: true, childList: true, characterData: true };
-    var sidebar_content = document.querySelector('#sidebar_content');
-    observer.observe(sidebar_content, config);
+    var sidebarContent = document.querySelector('#sidebar_content');
+    observer.observe(sidebarContent, config);
 }
 
 /**
@@ -81,13 +79,13 @@ function addSidebarObserver() {
  * when the user click the "Load more" button
  */
 function addChangesetsObserver() {
-    var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            if (mutation.type == 'attributes'){
+    var observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            if (mutation.type == 'attributes') {
                 return;
             }
-            search_changesets();
-        });    
+            searchChangesets();
+        });
     });
 
     var config = { attributes: true, childList: true, characterData: true };
