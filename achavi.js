091a2b44adb79b39980171b8acc1893b037f36a1
Adrián Nieto <adrian.nieto7@gmail.com>
2017-06-16 00:14:40 +0200

    Add button to changeset detail page

--- achavi.js
+++ achavi.js
@@ -2,7 +2,7 @@ var ACHAVI_URL = 'https://overpass-api.de/achavi/?changeset='
 
 function createLink(changeset_id) {
     var achavi_link = document.createElement("a")
-    achavi_link.text = "Achavi"
+    achavi_link.text = "Achavi";
     achavi_link.href = ACHAVI_URL + changeset_id;
     achavi_link.setAttribute('target', '_blank');
     achavi_link.style.background = "#7092FF";
@@ -33,8 +33,33 @@ function search_changesets() {
     }
 }
 
+function showButtonInChangesetDetailPage() {
+    console.log("showButtonInChangesetDetailPage");
+    var changeset_id = window.location.pathname.replace("/changeset/", "");
+
+    var details = document.querySelector('#sidebar_content h2');
+    var achavi_link = createLink(changeset_id);
+    details.appendChild(achavi_link);
+    console.log("aaa");
+}
+
+function scanPage() {
+    if (isChangesetDetailPage()) {
+        showButtonInChangesetDetailPage();
+    } else {
+        search_changesets();
+        addChangesetsObserver();
+    }
+}
+
+function isChangesetDetailPage() {
+    return window.location.pathname.indexOf("/changeset/") != -1;
+}
+
 /**
- * Add a listener to the sidebar changes
+ * Add a listener to the sidebar changes. It's needed to search for new changesets
+ * when the user clicks in the "History" button in the top bar or when the user clicks
+ * on a changeset
  */
 function addSidebarObserver() {
     var observer = new MutationObserver(function(mutations) {
@@ -42,18 +67,18 @@ function addSidebarObserver() {
             if (mutation.type == 'attributes'){
                 return;
             }
-            search_changesets();
-            addChangesetsObserver();
+            scanPage();
         });    
     });
 
     var config = { attributes: true, childList: true, characterData: true };
-    var changesets = document.querySelector('#sidebar_content');
-    observer.observe(changesets, config);
+    var sidebar_content = document.querySelector('#sidebar_content');
+    observer.observe(sidebar_content, config);
 }
 
 /**
- * Add a listener to the changeset list
+ * Add a listener to the changeset list. It's need to  search for new changesets 
+ * when the user click the "Load more" button
  */
 function addChangesetsObserver() {
     var observer = new MutationObserver(function(mutations) {
@@ -70,6 +95,5 @@ function addChangesetsObserver() {
     observer.observe(changesets, config);
 }
 
-search_changesets();
+scanPage();
 addSidebarObserver();
-addChangesetsObserver();
